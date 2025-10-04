ce38fa8e611929b3a96a5e82a100649937592c5a
rk-netizen <kaushik.irl@gmail.com>
2025-10-04 17:39:33 +0100

    route set up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './features/post-list/post-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/stories', pathMatch: 'full' },
+  { path: 'stories', component: PostListComponent },
+  { path: '**', redirectTo: '/stories' },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-import { NewsComponent } from './features/news/news.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [NewsComponent],
+  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
